8ea599bd44541c0e791380c92841389b400a64a6
Manuele Menozzi <manuele.menozzi@gmail.com>
2017-11-10 17:15:22 +0100

    Typo

--- bin/producer.php
+++ bin/producer.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $kernel = new \Webgriffe\Esb\Kernel();
 
-$pheanstalk = $kernel->getContainer()->get(\Pheanstalk\PheanstalkInterface::class)
+$pheanstalk = $kernel->getContainer()->get(\Pheanstalk\PheanstalkInterface::class);
 
 $pheanstalk
     ->useTube('testtube')
