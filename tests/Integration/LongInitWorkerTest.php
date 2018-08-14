fabbdc215bab11f5ef26ec1b193a6d456e9dbf7b
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-08-14 11:12:27 +0200

    Fixes a test failing due to additional logging of web console

--- tests/Integration/LongInitWorkerTest.php
+++ tests/Integration/LongInitWorkerTest.php
@@ -24,11 +24,12 @@ class LongInitWorkerTest extends KernelTestCase
         self::$kernel->boot();
 
         $logEntries = $this->logHandler()->getRecords();
-        $this->assertCount(4, $logEntries);
+        $this->assertCount(5, $logEntries);
         $this->assertContains('No producer to start', $logEntries[0]['formatted']);
-        $this->assertContains('Starting async job in long init worker...', $logEntries[1]['formatted']);
-        $this->assertContains('Async job done in long init worker, result is: done', $logEntries[2]['formatted']);
-        $this->assertContains('A Worker has been successfully initialized', $logEntries[3]['formatted']);
-        $this->assertContains('DummyLongInitWorker', $logEntries[3]['formatted']);
+        $this->assertContains('Web console server started.', $logEntries[1]['formatted']);
+        $this->assertContains('Starting async job in long init worker...', $logEntries[2]['formatted']);
+        $this->assertContains('Async job done in long init worker, result is: done', $logEntries[3]['formatted']);
+        $this->assertContains('A Worker has been successfully initialized', $logEntries[4]['formatted']);
+        $this->assertContains('DummyLongInitWorker', $logEntries[4]['formatted']);
     }
 }
