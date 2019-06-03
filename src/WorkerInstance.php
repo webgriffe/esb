5bf65934df12665e8e1918b15ab37abec4c5d72e
Fabiana Romagnoli <fromagnoli@webgriffe.com>
2019-06-03 12:07:31 +0200

    Make WorkerInstance final

--- src/WorkerInstance.php
+++ src/WorkerInstance.php
@@ -10,7 +10,7 @@ use Webgriffe\Esb\Model\FlowConfig;
 use Webgriffe\Esb\Model\QueuedJob;
 use function Amp\call;
 
-class WorkerInstance implements WorkerInstanceInterface
+final class WorkerInstance implements WorkerInstanceInterface
 {
     /**
      * @var FlowConfig
