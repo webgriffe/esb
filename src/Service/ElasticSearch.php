31af4db2d5970cea26c576a5c0ca944d853544fa
Manuele Menozzi <manuele.menozzi@gmail.com>
2019-12-02 12:21:32 +0100

    Try to handle ES no shard availble error during index (#5)

--- src/Service/ElasticSearch.php
+++ src/Service/ElasticSearch.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Webgriffe\Esb\Service;
 
 use Amp;
+use Generator;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
 use Webgriffe\AmpElasticsearch\Client;
 use Webgriffe\AmpElasticsearch\Error;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -16,6 +18,8 @@ use Webmozart\Assert\Assert;
 
 class ElasticSearch
 {
+    private const NO_SHARD_AVAILABLE_INDEX_MAX_RETRY = 10;
+
     /**
      * @var Client
      */
@@ -35,11 +39,7 @@ class ElasticSearch
     public function indexJob(JobInterface $job, string $indexName): Amp\Promise
     {
         return Amp\call(function () use ($job, $indexName) {
-            yield $this->client->indexDocument(
-                $indexName,
-                $job->getUuid(),
-                (array)$this->normalizer->normalize($job, 'json')
-            );
+            yield from $this->doIndexJob($job, $indexName, 0);
         });
     }
 
@@ -57,5 +57,33 @@ class ElasticSearch
             Assert::keyExists($response, '_source');
             return $this->normalizer->denormalize($response['_source'], Job::class, 'json');
         });
+    }
+
+    /**
+     * @param JobInterface $job
+     * @param string $indexName
+     * @param int $retry
+     * @return Generator
+     * @throws ExceptionInterface
+     */
+    private function doIndexJob(JobInterface $job, string $indexName, int $retry): Generator
+    {
+        try {
+            yield $this->client->indexDocument(
+                $indexName,
+                $job->getUuid(),
+                (array)$this->normalizer->normalize($job, 'json')
+            );
+        } catch (Error $error) {
+            $errorData = $error->getData();
+            $errorType = $errorData['error']['type'] ?? null;
+            if ($errorType === 'no_shard_available_action_exception' &&
+                $retry < self::NO_SHARD_AVAILABLE_INDEX_MAX_RETRY) {
+                yield Amp\delay(1000);
+                yield from $this->doIndexJob($job, $indexName, ++$retry);
+                return;
+            }
+            throw $error;
+        }
     }
 }
