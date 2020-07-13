a0b00cf5da827094dd7f99b33a5413fe02499359
Andrea Zambon <azambon@webgriffe.com>
2020-07-13 11:08:38 +0200

    WIP

--- src/Service/ElasticSearch.php
+++ src/Service/ElasticSearch.php
@@ -43,6 +43,13 @@ class ElasticSearch
         });
     }
 
+    public function bulkIndexJobs(array $jobs, string $indexName): Amp\Promise
+    {
+        return Amp\call(function () use ($jobs, $indexName) {
+            yield from $this->doBulkIndexJobs($jobs, $indexName, 0);
+        });
+    }
+
     public function fetchJob(string $uuid, string $indexName): Amp\Promise
     {
         return Amp\call(function () use ($uuid, $indexName) {
@@ -72,6 +79,35 @@ class ElasticSearch
      * @throws ExceptionInterface
      */
     private function doIndexJob(JobInterface $job, string $indexName, int $retry): Generator
+    {
+        try {
+            yield $this->client->indexDocument(
+                $indexName,
+                $job->getUuid(),
+                (array)$this->normalizer->normalize($job, 'json')
+            );
+        } catch (Error $error) {
+            $errorData = $error->getData();
+            $errorType = $errorData['error']['type'] ?? null;
+            if ($errorType === 'no_shard_available_action_exception' &&
+                $retry < self::NO_SHARD_AVAILABLE_INDEX_MAX_RETRY) {
+                // TODO Log no shard available retrials and refactor
+                yield Amp\delay(1000);
+                yield from $this->doIndexJob($job, $indexName, ++$retry);
+                return;
+            }
+            throw $error;
+        }
+    }
+
+    /**
+     * @param JobInterface[] $jobs
+     * @param string $indexName
+     * @param int $retry
+     * @return Generator
+     * @throws ExceptionInterface
+     */
+    private function doBulkIndexJobs(array $jobs, string $indexName, int $retry): Generator
     {
         try {
             yield $this->client->indexDocument(
