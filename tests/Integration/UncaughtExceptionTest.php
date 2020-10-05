82ceb70eb2d47eb20dc12092fb81cc7936d93816
Peter van der Wal <peter.vanderwal@youweagency.com>
2020-10-05 14:07:08 +0200

    Log all reasons from a Amp\MultiReasonException
    Instead of logging the message
        "Fatal error: Uncaught Amp\MultiReasonException: Multiple errors encountered; use Amp\MultiReasonException::getReasons() to retrieve the array of exceptions thrown in ~"
    log all individual exceptions so you can see what's actually going wrong

--- tests/Integration/UncaughtExceptionTest.php
+++ tests/Integration/UncaughtExceptionTest.php
@@ -2,15 +2,14 @@
 
 namespace Webgriffe\Esb\Integration;
 
+use Monolog\Logger;
 use Webgriffe\Esb\DummyFilesystemWorker;
 use Webgriffe\Esb\DummyRepeatProducer;
+use Webgriffe\Esb\MultiReasonInitFailingWorker;
 use Webgriffe\Esb\KernelTestCase;
 
 class UncaughtExceptionTest extends KernelTestCase
 {
-    /**
-     * @expectedException \Amp\Beanstalk\ConnectException
-     */
     public function testUncaughtExceptionIsLoggedAndThrown()
     {
         self::createKernel([
@@ -27,8 +26,45 @@ class UncaughtExceptionTest extends KernelTestCase
                 ]
             ]
         ]);
-        self::$kernel->boot();
 
-        $this->logHandler()->hasCriticalThatContains('An uncaught exception occurred, ESB will be stopped now!');
+        try {
+            $this->expectException(\Amp\Beanstalk\ConnectException::class);
+            self::$kernel->boot();
+        } finally {
+            $this->assertTrue($this->logHandler()->hasCriticalThatContains('An uncaught exception occurred, ESB will be stopped now!'));
+        }
+    }
+
+    public function testMultiReasonExceptionIsLoggedAndThrown()
+    {
+        self::createKernel([
+            'services' => [
+                DummyRepeatProducer::class => ['class' => DummyRepeatProducer::class, 'arguments' => []],
+                MultiReasonInitFailingWorker::class => [],
+            ],
+            'flows' => [
+                'sample_tube' => [
+                    'description' => 'Flow',
+                    'producer' => ['service' => DummyRepeatProducer::class],
+                    'worker' => ['service' => MultiReasonInitFailingWorker::class],
+                ]
+            ]
+        ]);
+
+        try {
+            $this->expectException(\Amp\MultiReasonException::class);
+            self::$kernel->boot();
+        } finally {
+            $this->assertTrue($this->logHandler()->hasRecordThatPasses(function ($record) {
+                $context = $record['context'];
+                // Verify log message
+                return $record['message'] === 'An uncaught exception occurred, ESB will be stopped now!'
+                        // Verify log context contains all reasons
+                        && isset($context['reasons']) && is_array($context['reasons'])
+                        && count($context['reasons']) === 2
+                        && $context['reasons'][0]['message'] === 'Exception number one'
+                        && $context['reasons'][1]['message'] === 'Exception number two';
+            }, Logger::CRITICAL));
+        }
     }
 }
