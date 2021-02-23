9aecb43835b4aa6a5cdcc5540fc31a6739a34b9d
Peter van der Wal <peter.vanderwal@youweagency.com>
2021-02-23 11:41:12 +0100

    Typehint JobNotFoundException::__construct($code)
    PhpStan: Method Webgriffe\Esb\Exception\ElasticSearch\JobNotFoundException::__construct() has parameter $code with no typehint specified.

--- src/Exception/ElasticSearch/JobNotFoundException.php
+++ src/Exception/ElasticSearch/JobNotFoundException.php
@@ -8,7 +8,7 @@ use Throwable;
 
 class JobNotFoundException extends \RuntimeException
 {
-    public function __construct(string $jobUuid, $code = 0, Throwable $previous = null)
+    public function __construct(string $jobUuid, int $code = 0, Throwable $previous = null)
     {
         parent::__construct(sprintf('Job with UUID "%s" has not been found.', $jobUuid), $code, $previous);
     }
