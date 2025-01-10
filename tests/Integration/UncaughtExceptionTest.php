e5109b8597220694a86fbee3cec0bb74dc79e0dd
Andrea Zambon <azambon@webgriffe.com>
2025-01-10 16:04:41 +0100

    Updates a test not to use the old annotation for exception handling

--- tests/Integration/UncaughtExceptionTest.php
+++ tests/Integration/UncaughtExceptionTest.php
@@ -2,32 +2,38 @@
 
 namespace Webgriffe\Esb\Integration;
 
+use Amp\Beanstalk\ConnectException;
 use Webgriffe\Esb\DummyFilesystemWorker;
 use Webgriffe\Esb\DummyRepeatProducer;
 use Webgriffe\Esb\KernelTestCase;
 
 class UncaughtExceptionTest extends KernelTestCase
 {
-    /**
-     * @expectedException \Amp\Beanstalk\ConnectException
-     */
     public function testUncaughtExceptionIsLoggedAndThrown()
     {
-        self::createKernel([
-            'parameters' => ['beanstalkd' => 'tcp://invalid-host:11300'],
-            'services' => [
-                DummyRepeatProducer::class => ['class' => DummyRepeatProducer::class, 'arguments' => []],
-                DummyFilesystemWorker::class => ['arguments' => ['/dev/null']],
-            ],
-            'flows' => [
-                'sample_tube' => [
-                    'description' => 'Flow',
-                    'producer' => ['service' => DummyRepeatProducer::class],
-                    'worker' => ['service' => DummyFilesystemWorker::class],
+        try {
+            self::createKernel([
+                'parameters' => ['beanstalkd' => 'tcp://invalid-host:11300'],
+                'services' => [
+                    DummyRepeatProducer::class => ['class' => DummyRepeatProducer::class, 'arguments' => []],
+                    DummyFilesystemWorker::class => ['arguments' => ['/dev/null']],
+                ],
+                'flows' => [
+                    'sample_tube' => [
+                        'description' => 'Flow',
+                        'producer' => ['service' => DummyRepeatProducer::class],
+                        'worker' => ['service' => DummyFilesystemWorker::class],
+                    ]
                 ]
-            ]
-        ]);
-        self::$kernel->boot();
+            ]);
+            self::$kernel->boot();
+
+            $this->fail('Expected exception was not thrown');
+        } catch (ConnectException $e) {
+            //All as expected
+        } catch (\Throwable $e) {
+            throw $e;
+        }
 
         $this->logHandler()->hasCriticalThatContains('An uncaught exception occurred, ESB will be stopped now!');
     }
